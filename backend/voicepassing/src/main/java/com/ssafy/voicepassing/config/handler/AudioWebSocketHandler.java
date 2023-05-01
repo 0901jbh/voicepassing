9e9a8fe9125971eb19bdfcd77457eb6a783b6d8f
박정희 <wjdgmll77@naver.com>
2023-05-01 09:35:13 +0900

    Feat : RestAPI 유틸 추가, untrunc서버와 통신확인

--- backend/voicepassing/src/main/java/com/ssafy/voicepassing/config/handler/AudioWebSocketHandler.java
+++ backend/voicepassing/src/main/java/com/ssafy/voicepassing/config/handler/AudioWebSocketHandler.java
@@ -4,50 +4,71 @@ package com.ssafy.voicepassing.config.handler;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.socket.BinaryMessage;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 
+import com.ssafy.voicepassing.util.RestAPIUtil;
+
 
 @Component
 public class AudioWebSocketHandler extends AbstractWebSocketHandler {
 	private static final Logger logger = LoggerFactory.getLogger(AudioWebSocketHandler.class);
+	
+	@Autowired
+	private RestAPIUtil restApiUtil;
+	
+	
 	@Value("${SPRING_RECORD_TEMP_DIR}")
 	private String RECORD_PATH;
+	
+	@Value("DOMAIN_UNTRUNC")
+	private String DOMAIN_UNTRUNC;
+	
+	
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		super.afterConnectionEstablished(session);
 		File directory = new File(RECORD_PATH+"/"+session.getId());
         if (!directory.exists()) {
             directory.mkdir();
-            System.out.println("디렉터리 생성 완료");
+            logger.info("디렉터리 생성 완료");
         } else {
-            System.out.println("이미 디렉터리가 존재합니다.");
+        	logger.info("이미 디렉터리가 존재합니다.");
         }
 		logger.info("소켓연결시작: {}", session.getId());
 	}
 
 	@Override
-	public void handleBinaryMessage(WebSocketSession session, BinaryMessage message) {
+	public void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws Exception{
 		logger.info("bin sessiong request: {}", session.getId());
 		ByteBuffer byteBuffer = message.getPayload();
-		try (FileOutputStream outputStream = new FileOutputStream(RECORD_PATH + "/" + session.getId() + "/record.m4a",true)) {
-			byte[] bytes = new byte[byteBuffer.remaining()];
-			byteBuffer.get(bytes);
-			outputStream.write(bytes);
-			outputStream.close();
-			
-		}catch (Exception e) {
-			logger.error("Error saving file", e);
-		}
+		
+		FileOutputStream outputStream = new FileOutputStream(RECORD_PATH + "/" + session.getId() + "/record.m4a",true);
+		byte[] bytes = new byte[byteBuffer.remaining()];
+		byteBuffer.get(bytes);
+		outputStream.write(bytes);
+		outputStream.close();
 
+		String URL = DOMAIN_UNTRUNC+"/recover";
+		Map<String, String> params = new HashMap<>();
+		params.put("sessionId", session.getId());
+		
+		Map<String, Object> result = restApiUtil.requestGet(URL, params);
+		logger.info("결과 : {}", result);
 	}
 
 	@Override
@@ -60,9 +81,9 @@ public class AudioWebSocketHandler extends AbstractWebSocketHandler {
 		File directoryToDelete = new File(RECORD_PATH+"/"+session.getId());
 		if (directoryToDelete.exists()) {
             deleteDirectory(directoryToDelete);
-            System.out.println("디렉터리 삭제 완료");
+            logger.info("디렉터리 삭제 완료");
         } else {
-            System.out.println("삭제할 디렉터리가 존재하지 않습니다.");
+        	logger.info("삭제할 디렉터리가 존재하지 않습니다.");
         }
 		logger.info("소켓연결해제: {}", session.getId());
 		
