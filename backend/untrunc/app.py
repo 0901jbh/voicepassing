5ac6a3b764e30eaa6a6c94d11e4ddcae0b6f6a7c
박정희 <wjdgmll77@naver.com>
2023-05-01 09:33:39 +0900

    Refactor : untrunc도메인 환경변수로 설정, 주고받는 형태 객체로 받을 수 있도록 설정

--- backend/untrunc/app.py
+++ backend/untrunc/app.py
@@ -4,18 +4,23 @@ import subprocess
 app = Flask(__name__)
 @app.route('/alive', methods=['GET'])
 def aliveTest():
-    return "ALIVE"
+    return {"msg": "ALIVE"}
 
 
-@app.route('/recover', methods=['POST'])
+@app.route('/recover', methods=['GET'])
 def recoverM4A():
-    session_id = request.form['sessionId']
-    try:
-        subprocess.run(["untrunc", f"/data/ok.m4a", f"/data/{session_id}/record.m4a"], check=True)
-        subprocess.run(["mv", f"/data/{session_id}/record.m4a_fixed.m4a", f"/data/{session_id}/recover.m4a"], check=True)
-        return {"msg": "성공"}
-    except subprocess.CalledProcessError:
-        return {"msg": "실패"}
+    params = request.args.to_dict()
+    session_id = params["sessionId"]
+    
+    if session_id:
+        try:
+            subprocess.run(["untrunc", f"/data/ok.m4a", f"/data/{session_id}/record.m4a"], check=True)
+            subprocess.run(["mv", f"/data/{session_id}/record.m4a_fixed.m4a", f"/data/{session_id}/recover.m4a"], check=True)
+            return {"msg": "성공"}
+        except subprocess.CalledProcessError:
+            return {"msg": "실패"}
+    else:
+        return {"msg": "세션없음"}
 
 if __name__ == '__main__':
     print("start_flask")
