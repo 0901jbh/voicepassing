213e923c7016456ada537342fb5c0057f083e40f
박정희 <wjdgmll77@naver.com>
2023-05-03 14:41:23 +0900

    Feat : 클로바 요청 전 로그처리

--- backend/untrunc/app.py
+++ backend/untrunc/app.py
@@ -17,19 +17,21 @@ def recoverM4A():
     if session_id:
         try:
             DATA_PATH = os.environ.get("FLASK_DATA_PATH","/data/WebSocket")
+            FLASK_FILE_PERIOD = int(os.environ.get("FLASK_FILE_PERIOD","10000"))
+            FLASK_FILE_DUPLICATE = int(os.environ.get("FLASK_FILE_DUPLICATE","1000"))
             subprocess.run(["untrunc", f"{DATA_PATH}/ok.m4a", f"{DATA_PATH}/{session_id}/record.m4a"], check=True)
             subprocess.run(["mv", f"{DATA_PATH}/{session_id}/record.m4a_fixed.m4a", f"{DATA_PATH}/{session_id}/recover.m4a"], check=True)
             target = f"{DATA_PATH}/{session_id}/recover.m4a"
             partition_folder = f"{DATA_PATH}/{session_id}/part"
             audio_file = AudioSegment.from_file(target, format="m4a")
-            window_size = 5000
+            window_size = FLASK_FILE_PERIOD
             cnt = 0
 
             new_file = []
             for i in range(0,len(audio_file),window_size):
                 if not os.path.isfile(f"{partition_folder}/{cnt}.mp3"):
-                    target = audio_file[i:i+window_size+1000]
-                    if len(target)==(window_size+1000):
+                    target = audio_file[i:i+window_size+FLASK_FILE_DUPLICATE]
+                    if len(target)==(window_size+FLASK_FILE_DUPLICATE):
                         target.export(f"{partition_folder}/{cnt}.mp3", format="mp3")
                         new_file.append(f"{cnt}.mp3")
                 cnt += 1
--- backend/voicepassing/src/main/java/com/ssafy/voicepassing/config/handler/AudioWebSocketHandler.java
+++ backend/voicepassing/src/main/java/com/ssafy/voicepassing/config/handler/AudioWebSocketHandler.java
@@ -1,11 +1,11 @@
 package com.ssafy.voicepassing.config.handler;
 
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -19,92 +19,101 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 
+import com.ssafy.voicepassing.model.service.AnalysisService;
 import com.ssafy.voicepassing.util.RestAPIUtil;
 
 @Component
 public class AudioWebSocketHandler extends AbstractWebSocketHandler {
 	private static final Logger logger = LoggerFactory.getLogger(AudioWebSocketHandler.class);
-	
+
 	@Autowired
 	private RestAPIUtil restApiUtil;
-	
-	
+
+	@Autowired
+	private AnalysisService analysisService;
+
 	@Value("${SPRING_RECORD_TEMP_DIR}")
 	private String RECORD_PATH;
-	
+
 	@Value("${DOMAIN_UNTRUNC}")
 	private String DOMAIN_UNTRUNC;
-	
-	
+
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		super.afterConnectionEstablished(session);
-		
-		File directory = new File(RECORD_PATH+"/"+session.getId());
-        if (!directory.exists()) {
-            directory.mkdir();
-			File partDirectory = new File(RECORD_PATH+"/"+session.getId()+"/part");
+
+		File directory = new File(RECORD_PATH + "/" + session.getId());
+		if (!directory.exists()) {
+			directory.mkdir();
+			File partDirectory = new File(RECORD_PATH + "/" + session.getId() + "/part");
 			partDirectory.mkdir();
-            logger.info("디렉터리 생성 완료");
-        } else {
-        	logger.info("이미 디렉터리가 존재합니다.");
-        }
+			logger.info("디렉터리 생성 완료");
+		} else {
+			logger.info("이미 디렉터리가 존재합니다.");
+		}
 		logger.info("소켓연결시작: {}", session.getId());
 	}
 
 	@Override
-	public void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws Exception{
+	public void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws Exception {
 		logger.info("bin sessiong request: {}", session.getId());
 		ByteBuffer byteBuffer = message.getPayload();
-		
-		FileOutputStream outputStream = new FileOutputStream(RECORD_PATH + "/" + session.getId() + "/record.m4a",true);
+
+		FileOutputStream outputStream = new FileOutputStream(RECORD_PATH + "/" + session.getId() + "/record.m4a", true);
 		byte[] bytes = new byte[byteBuffer.remaining()];
 		byteBuffer.get(bytes);
 		outputStream.write(bytes);
 		outputStream.close();
-		
-		logger.info("URL : {}",DOMAIN_UNTRUNC);
-		String URL = DOMAIN_UNTRUNC+"/recover";
+
+		logger.info("URL : {}", DOMAIN_UNTRUNC);
+		String URL = DOMAIN_UNTRUNC + "/recover";
 		Map<String, String> params = new HashMap<>();
 		params.put("sessionId", session.getId());
-		
+
 		Map<String, Object> result = restApiUtil.requestGet(URL, params);
 		logger.info("결과 : {}", result);
+
+		List<String> newFile = (List<String>) result.get("new_file");
+		for (int i = 0; i < newFile.size(); i++) {
+			//String clovaResult = analysisService.SpeechToText(session.getId(), newFile.get(i));
+			String clovaResult = session.getId()+" "+ newFile.get(i);//테스트용
+			
+			logger.info("클로바 요청{} : {}",i,clovaResult);
+		}
 	}
 
 	@Override
 	public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 		logger.info("소켓텍스트: {}", message);
 		logger.info("바이너리 변환: {}", message.getPayload().getBytes("utf-8"));
-		
+
 	}
 
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-		File directoryToDelete = new File(RECORD_PATH+"/"+session.getId());
+		File directoryToDelete = new File(RECORD_PATH + "/" + session.getId());
 		if (directoryToDelete.exists()) {
-            deleteDirectory(directoryToDelete);
-            logger.info("디렉터리 삭제 완료");
-        } else {
-        	logger.info("삭제할 디렉터리가 존재하지 않습니다.");
-        }
+			deleteDirectory(directoryToDelete);
+			logger.info("디렉터리 삭제 완료");
+		} else {
+			logger.info("삭제할 디렉터리가 존재하지 않습니다.");
+		}
 		logger.info("소켓연결해제: {}", session.getId());
-		
+
 	}
-	
-	
-    // 디렉터리 삭제 메서드
-    public static void deleteDirectory(File directory) {
-        File[] files = directory.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                if (file.isDirectory()) {
-                    deleteDirectory(file);
-                } else {
-                    file.delete();
-                }
-            }
-        }
-        directory.delete();
-    }
+
+	// 디렉터리 삭제 메서드
+	public static void deleteDirectory(File directory) {
+		File[] files = directory.listFiles();
+		if (files != null) {
+			for (File file : files) {
+				if (file.isDirectory()) {
+					deleteDirectory(file);
+				} else {
+					file.delete();
+				}
+			}
+		}
+		directory.delete();
+	}
 }
