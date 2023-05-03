3ac46858adedd9b5ea63db75f71b982db17c2dc8
박정희 <wjdgmll77@naver.com>
2023-05-03 10:58:45 +0900

    Feat : untrunc 리턴값 표시추가

--- backend/untrunc/app.py
+++ backend/untrunc/app.py
@@ -24,14 +24,17 @@ def recoverM4A():
             audio_file = AudioSegment.from_file(target, format="m4a")
             window_size = 5000
             cnt = 0
+
+            new_file = []
             for i in range(0,len(audio_file),window_size):
                 if not os.path.isfile(f"{partition_folder}/{cnt}.mp3"):
                     target = audio_file[i:i+window_size+1000]
                     if len(target)==(window_size+1000):
                         target.export(f"{partition_folder}/{cnt}.mp3", format="mp3")
+                        new_file.append(cnt)
                 cnt += 1
 
-            return {"msg": "성공"}
+            return {"msg": True, "new_file": new_file }
         except subprocess.CalledProcessError:
             return {"msg": "실패"}
     else:
