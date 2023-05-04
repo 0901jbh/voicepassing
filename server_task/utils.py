5128decea85ad91b181ce8d101384fbc438bf492
veonico <veonico@naver.com>
2023-05-04 14:57:15 +0900

    Fix : Flag가 오지 않더라도 여러 문장으로 처리하도록 수정
    Former-commit-id: 1977ed6331be2fdfd7d7679a58467a4848ea4537

--- server_task/utils.py
+++ server_task/utils.py
@@ -5,7 +5,7 @@ def classify_phonecall(results : List):
     # 가중 평균을 통해 통화 내용을 판단
 
     label_probs = torch.FloatTensor(results)
-    weights = torch.range(start = len(label_probs), end = 1, step = -1)
+    weights = torch.arange(start = len(label_probs), end = 0, step = -1)
 
     weighted_probs = torch.sum((label_probs.T*weights).T, axis = 0)/torch.sum(weights)
     label = torch.argmax(weighted_probs).item()
