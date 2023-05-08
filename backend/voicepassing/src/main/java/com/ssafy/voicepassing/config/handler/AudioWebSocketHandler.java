0f57730e86e180dcbf750ddc6dc678d1b09240df
박정희 <wjdgmll77@naver.com>
2023-05-08 13:15:42 +0900

    Feat : 플러터와 양방향 전송, 교류까지
    Former-commit-id: 0ca42a91c1f9bb40739b84e510983d0f98148613

--- backend/voicepassing/src/main/java/com/ssafy/voicepassing/config/handler/AudioWebSocketHandler.java
+++ backend/voicepassing/src/main/java/com/ssafy/voicepassing/config/handler/AudioWebSocketHandler.java
@@ -48,9 +48,7 @@ public class AudioWebSocketHandler extends AbstractWebSocketHandler {
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		super.afterConnectionEstablished(session);
-		Map<String, Object> sessionAttribute = session.getAttributes();
-		sessionAttribute.put("start", 0);
-		sessionAttribute.put("end", 0);
+		createFolder(session.getId());
 	}
 
 	@Override
@@ -60,8 +58,8 @@ public class AudioWebSocketHandler extends AbstractWebSocketHandler {
 
 		appendFile(message.getPayload(), session.getId());
 
-		int startValue = (int) sessionAttribute.get("start");
-		sessionAttribute.put("start", startValue + 1);
+//		int startValue = (int) sessionAttribute.get("start");
+//		sessionAttribute.put("start", startValue + 1);
 
 		logger.info("GET 요청 (복원): {}", DOMAIN_UNTRUNC + "/recover");
 
@@ -76,41 +74,22 @@ public class AudioWebSocketHandler extends AbstractWebSocketHandler {
 		// 15초단위 추가 파일
 		List<String> newFile = (List<String>) untruncResult.get("new_file");
 		String newFilePath = RECORD_PATH + "/" + session.getId() + "/part/";
-		sendAiServer(newFile, newFilePath, session);
+		sendAiServer(newFile, newFilePath, session, false);
 
-		int endValue = (int) sessionAttribute.get("end");
-		sessionAttribute.put("start", endValue + 1);
 	}
 
 	@Override
 	public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 		logger.info("socket 정보 전달받음 : {}", message);
-
 		Gson gson = new Gson();
 		Map<String, Object> messageMap = gson.fromJson(message.getPayload(), Map.class);
-
-		int stateValue = (Integer) messageMap.get("state");
-
+		int stateValue = (int)Math.floor((double)messageMap.get("state"));
+		String androidId = (String)messageMap.get("androidId");
 		switch (stateValue) {
-		case 0:
-			logger.info("통화 받음 폴더 생성 : {}", session.getId());
-			createFolder(session.getId());
+		case 0: 
+			logger.info("Send session info : {} , androidId : {}", session.getId(), androidId);
 			break;
 		case 1:
-			logger.info("상태확인 start,end가 일치해야 이전과정이 끝났다는 것");
-			Map<String, Object> sessionAttribute = session.getAttributes();
-			int start = (int) sessionAttribute.get("start");
-			int end = (int) sessionAttribute.get("end");
-			Map<String, Boolean> result = new HashMap<>();
-			result.put("isFinish", start == end);
-
-			String jsonResult = gson.toJson(result);
-			TextMessage resultMessage = new TextMessage(jsonResult);
-			session.sendMessage(resultMessage);
-
-			break;
-		case 2:
-			logger.info("통화 종료");
 			String untruncUrl = DOMAIN_UNTRUNC + "/recover";
 			Map<String, String> params = new HashMap<>();
 			params.put("sessionId", session.getId());
@@ -119,18 +98,7 @@ public class AudioWebSocketHandler extends AbstractWebSocketHandler {
 
 			List<String> newFile = (List<String>) untruncResult.get("new_file");
 			String newFilePath = RECORD_PATH + "/" + session.getId() + "/part/";
-			sendAiServer(newFile, newFilePath, session);
-			//여기까지 15초 단위 마지막 부분 전부 처리
-			
-			
-			//여기에 ai분석 마지막 요약 전송하기
-			String aiDomain = AI_SERVER_URI + "/inference";
-			MultiValueMap<String, Object> mybody = new LinkedMultiValueMap<>();
-			mybody.add("sessionId", session.getId());
-			mybody.add("isFinish", true);
-			mybody.add("text", "이상입니다.");
-			Map<String, Object> aiResult = restApiUtil.requestPost(aiDomain, mybody);
-			sendClient(session, aiResult);
+			sendAiServer(newFile, newFilePath, session,true);
 		default:
 			logger.info("error");
 			break;
@@ -169,7 +137,7 @@ public class AudioWebSocketHandler extends AbstractWebSocketHandler {
 	}
 
 	// 새로생성된 part파일을 분석하고 보내는 것 까지
-	public void sendAiServer(List<String> newFile, String newFilePath, WebSocketSession session) throws Exception {
+	public void sendAiServer(List<String> newFile, String newFilePath, WebSocketSession session,Boolean isFinish) throws Exception {
 		for (int i = 0; i < newFile.size(); i++) {
 
 			String filePath = newFilePath + newFile.get(i);
@@ -177,12 +145,13 @@ public class AudioWebSocketHandler extends AbstractWebSocketHandler {
 			MultiValueMap<String, Object> mybody = new LinkedMultiValueMap<>();
 			mybody.add("sessionId", session.getId());
 			mybody.add("filepath", filePath);
-			mybody.add("isFinish", 5);
+			mybody.add("isFinish", (isFinish&&i==newFile.size()-1));
 
-			logger.info("클로바 요청{} 시작: {}", filePath);
+			logger.info("클로바 요청 {} 시작 {} , {}: ", i , filePath,mybody);
 			Map<String, Object> myResult = restApiUtil.requestPost(myUrl, mybody);
+			myResult.put("isFinish", isFinish&&i==newFile.size()-1);
 			sendClient(session, myResult);
-			logger.info("클로바 요청{} 결과: {}", i, myResult);
+			logger.info("클로바 요청 {} 결과: {}", i, myResult);
 		}
 	}
 
