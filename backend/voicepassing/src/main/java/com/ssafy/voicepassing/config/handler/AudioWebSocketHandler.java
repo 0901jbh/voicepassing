b481714918f27c8003c07cb15302489c0d854334
박정희 <wjdgmll77@naver.com>
2023-05-08 13:32:42 +0900

    Style : 스위치 사용에 break 누락 수정
    Former-commit-id: c48acd456ccde9c9aba611581e90d3b9e80edd65

--- backend/voicepassing/src/main/java/com/ssafy/voicepassing/config/handler/AudioWebSocketHandler.java
+++ backend/voicepassing/src/main/java/com/ssafy/voicepassing/config/handler/AudioWebSocketHandler.java
@@ -99,6 +99,7 @@ public class AudioWebSocketHandler extends AbstractWebSocketHandler {
 			List<String> newFile = (List<String>) untruncResult.get("new_file");
 			String newFilePath = RECORD_PATH + "/" + session.getId() + "/part/";
 			sendAiServer(newFile, newFilePath, session,true);
+			break;
 		default:
 			logger.info("error");
 			break;
