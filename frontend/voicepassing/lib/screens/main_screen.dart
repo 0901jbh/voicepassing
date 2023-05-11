1d848b8f15ed2f2cbb3601197db5b098f67d5eb1
jbh <0901jbh@naver.com>
2023-05-11 17:57:38 +0900

    feat:: upload front

--- frontend/voicepassing/lib/screens/main_screen.dart
+++ frontend/voicepassing/lib/screens/main_screen.dart
@@ -278,11 +278,11 @@ class _MainScreenState extends State<MainScreen> {
               const SizedBox(
                 height: 30,
               ),
-              const SizedBox(
+              SizedBox(
                 width: 315,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
+                  children: const [
                     ImgButton(
                       title: '검사 결과',
                       imgName: 'ResultImg',
@@ -298,11 +298,11 @@ class _MainScreenState extends State<MainScreen> {
               const SizedBox(
                 height: 10,
               ),
-              const SizedBox(
+              SizedBox(
                 width: 315,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
+                  children: const [
                     ImgButton(
                         title: '검색',
                         imgName: 'SearchImg',
--- frontend/voicepassing/lib/widgets/alarm_widget/call_ended_result_widget.dart
+++ frontend/voicepassing/lib/widgets/alarm_widget/call_ended_result_widget.dart
@@ -51,8 +51,8 @@ class _CallEndedResultWidgetState extends State<CallEndedResultWidget> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    const Row(
-                      children: [
+                    Row(
+                      children: const [
                         Image(
                           image: AssetImage('images/VoiceLogo.png'),
                           height: 22,
