e7762e3547dea447451467cca5a616c3370230d6
김용현 <dongind10@gmail.com>
2023-05-16 10:54:00 +0900

    In Progress : Background

--- frontend/voicepassing/lib/main.dart
+++ frontend/voicepassing/lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:flutter_background_service_android/flutter_background_service_android.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:voicepassing/routes.dart';
 import 'package:voicepassing/screens/main_screen.dart';
 // import 'package:voicepassing/services/notification_controller.dart';
@@ -13,6 +18,47 @@ void main() async {
   runApp(
     const App(),
   );
+}
+
+Future<void> initializeService() async {
+  final service = FlutterBackgroundService();
+
+  await service.configure(
+    iosConfiguration: IosConfiguration(
+      autoStart: false,
+    ),
+    androidConfiguration: AndroidConfiguration(
+      onStart: onStart,
+      isForegroundMode: true,
+      autoStart: true,
+    ),
+  );
+
+  service.startService();
+}
+
+@pragma('vm:entry-point')
+void onStart(ServiceInstance service) async {
+  DartPluginRegistrant.ensureInitialized();
+
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+  await preferences.setString("hello", "world");
+
+  if (service is AndroidServiceInstance) {
+    service.on('setAsForeground').listen((event) {
+      service.setAsForegroundService();
+    });
+
+    service.on('setAsBackground').listen((event) {
+      service.setAsBackgroundService();
+    });
+  }
+
+  service.on('stopService').listen((event) {
+    service.stopSelf();
+  });
+
+  debugPrint('Flutter Background Service');
 }
 
 // 오버레이 위젯 설정
@@ -43,6 +89,7 @@ class App extends StatelessWidget {
       },
       home: Scaffold(
         body: MainScreen(),
+        // body: Column(children: [children: ],)
       ),
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
