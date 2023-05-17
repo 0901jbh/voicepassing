f0f3844d3a4d722ffd849137feeda73e730882a9
김용현 <dongind10@gmail.com>
2023-05-17 14:51:03 +0900

    In Progress : list item 제작

--- frontend/voicepassing/lib/widgets/realtime_widget/list_item_widget.dart
+++ frontend/voicepassing/lib/widgets/realtime_widget/list_item_widget.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:voicepassing/style/color_style.dart';
 
 class ListItemWidget extends StatelessWidget {
   const ListItemWidget({super.key, required jsonData});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+        height: 80,
+        width: double.infinity,
+        color: ColorStyles.themeRed,
+        child: const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "위험",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+              ),
+            ),
+            Text("키워드",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12,
+                ))
+          ],
+        ));
   }
 }
