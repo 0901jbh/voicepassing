38aebbd6f2af544ad1f5865d3f7a7c393f41d10b
veonico <veonico@naver.com>
2023-05-18 16:21:44 +0900

    hotfix : tokenizer가 대명사 추출 안 하도록 수정
    Former-commit-id: cb3ebc8e7e07fe4ff437fb5b66d82205d62844fc

--- server_task/classifier/model.py
+++ server_task/classifier/model.py
@@ -172,14 +172,14 @@ class bayesianClassifierPipeLine():
                 final_tokens.append(token)
                 pre_morph = morph
 
-                if morph in {"NNG", "NNP", "NP"}:
+                if morph in {"NNG", "NNP"}:
                     noun_locs.append(len(final_tokens)-1)
                 continue
 
             # final_tokens가 비어있지 않을 때
-            if morph in {"NNG", "NNP", "NP"}: # 이번 토큰이 명사일 때
+            if morph in {"NNG", "NNP"}: # 이번 토큰이 명사일 때
 
-                if pre_morph in {"NNG", "NNP", "NP"}: # 이전 토큰도 명사일 때
+                if pre_morph in {"NNG", "NNP"}: # 이전 토큰도 명사일 때
                     last_token = final_tokens.pop()
                     final_tokens.append(last_token + token)
 
