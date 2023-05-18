fix : threshold 및 scaling 함수 수정
Former-commit-id: b9ffc5bd8004872de8810f9afb89a38ab9045a47

server_task/utils.py
@@ -13,7 +13,7 @@ def classify_phonecall(results : List | None):
         label = torch.argmax(weighted_probs).item()
 
         # 0.3을 넘지 않으면, 그냥 문제 없다고 판단
-        if weighted_probs[label] < 0.3:
+        if weighted_probs[label] < 0.5:
             label = 0
 
     else:
@@ -23,6 +23,6 @@ def classify_phonecall(results : List | None):
     return weighted_probs, label
 
 def threshold_scaling(value : float):
-    return value * ((1.5 * value - 1.5)**2 + 1)
+    return value * ((0.9 * value - 0.9)**2 + 1)
 
     